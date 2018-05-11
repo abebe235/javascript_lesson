73d33668c0a2cadff12911945f99af5dd4f41e69
F.abebe <musami633@gmail.com>
2018-05-11 11:55:29 +0900

    <js>変数について

---
+++ javascript20180511/js/common.js
@@ -0,0 +1,25 @@
+console.log('test') ;
+
+var test = 'test1' ;
+console.log(test) 
+var num = 1 ;
+console.log(num) ;
+var num2 = 2 ;
+console.log(num2) ;
+
+console.log(test+num2) ;
+
+console.log(1 + 1) ;
+console.log(1 - 1) ;
+console.log(2 * 3) ;　//掛け算
+console.log(4 / 2) ; //割り算
+console.log(9 % 2) ;　//余り
+
+num += 2 ; // num=num+2と一緒
+console.log(num) ;
+num -= 2 ;　//num = num - 2と一緒
+
+num++ ;
+num-- ;
+num++ ;
+console.log(num) ;
